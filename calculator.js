943ee83544d51b146101ae5ec6730033d5da659d
nokkvikarlsson <nokkvi17@ru.is>
2018-10-08 13:30:22 +0000

    implemented that calculator returns the sum of 2 numbers

--- calculator.js
+++ calculator.js
@@ -1,8 +1,15 @@
 function add (number){
-    if(number == "")
+    if(number == ""){
         return 0;
-
-    return parseInt(number);
+    }
+    
+    if(number.includes(",")){
+        var numberArray = number.split(",");
+        return parseInt(numberArray[0]) + parseInt(numberArray[1]);
+    }
+    else{
+        return parseInt(number);
+    } 
 }
 
 module.exports = add;
